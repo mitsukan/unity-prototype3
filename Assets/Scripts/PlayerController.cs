f69a94fe83b4e7c65e7b17c6ae56879c19df8408
Jihin Ip <jim_ip20@hotmail.com>
2022-07-13 17:52:26 +0100

    Making the character jump when the game starts

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,10 +4,15 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    // Define a variable of rigidbody type
+    private Rigidbody playerRigidBody;
     // Start is called before the first frame update
     void Start()
     {
-        
+        // assign the rigidbody component to the variable
+        playerRigidBody = GetComponent<Rigidbody>();
+        // The variable now has all of the methods for a rigibody, including add force
+        playerRigidBody.AddForce(Vector3.up * 500);
     }
 
     // Update is called once per frame
