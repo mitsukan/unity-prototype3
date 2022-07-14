2b06e08aabfcd7e6250e99ea5a504d9544e3ddeb
Jihin Ip <jim_ip20@hotmail.com>
2022-07-14 16:01:24 +0100

    adding obstacle prefab, adding move left script to obstacle and background

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,6 +8,7 @@ public class PlayerController : MonoBehaviour
     private Rigidbody playerRigidBody;
     public float jumpForce = 10;
     public float gravityModifier = 1;
+    public bool isOnGround = true;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,12 +20,18 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround == true)
         {
             // The variable now has all of the methods for a rigibody, including add force
             // Adding a mode to the force, Impulse means that the force is applied immediately
             // instead of overtime. This means that the force applied can be less
             playerRigidBody.AddForce(Vector3.up * jumpForce , ForceMode.Impulse);
+            isOnGround = false;
         }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        isOnGround = true;
     }
 }
